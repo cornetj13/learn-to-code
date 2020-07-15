ab38ea843994a0fc27dbdc964a8ec5b920413e94
Cornetj2 <cornetj2@students.wwu.edu>
2020-07-14 20:29:18 -0700

    Started the intermediate C# course.

---
+++ CarClassLibrary/CarClassLibrary/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClassLibrary 
+{
+    public class Car 
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+            this.Make = "None";
+            this.Model = "None";
+            this.Price = 0.00M;
+        }
+
+        public Car(string a, string b, decimal c)
+        {
+            Make = a;
+            Model = b;
+            Price = c;
+        }
+    }
+}
---
+++ CarClassLibrary/CarShopConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CarClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopConsoleApp
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Car c = new Car();
+            Car d = new Car("Ford", "Mustang", 9200);
+
+            Console.WriteLine("Car c is as follows: " + c.Make + " " + c.Model + " " + c.Price);
+            Console.WriteLine("Car d is as follows: " + d.Make + " " + d.Model + " " + d.Price);
+            Console.ReadLine();
+        }
+    }
+}
