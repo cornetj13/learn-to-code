bc055ba05cf5e42ed330b65fa71698b0867287ba
Cornetj2 <cornetj2@students.wwu.edu>
2020-07-15 22:39:40 -0700

    Add Basic Shopping Store Feature.

--- CarClassLibrary/CarShopConsoleApp/Program.cs
+++ CarClassLibrary/CarShopConsoleApp/Program.cs
@@ -11,11 +11,21 @@ namespace CarShopConsoleApp
     {
         static void Main(string[] args) 
         {
-            Car c = new Car();
+            Car c = new Car("Wayne Industries", "Batmobile", 1340000);
             Car d = new Car("Ford", "Mustang", 9200);
 
             Console.WriteLine("Car c is as follows: " + c.Make + " " + c.Model + " " + c.Price);
             Console.WriteLine("Car d is as follows: " + d.Make + " " + d.Model + " " + d.Price);
+
+            Store myStore = new Store();
+
+            myStore.ShoppingList.Add(c);
+            myStore.ShoppingList.Add(d);
+
+            decimal totalShoppingCost = myStore.Checkout();
+
+            Console.WriteLine("The Shopping List Total is: " + totalShoppingCost);
+
             Console.ReadLine();
         }
     }
