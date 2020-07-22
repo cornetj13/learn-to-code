46936afa5982fe8b8dc9479246015faa707fe1e7
cornetj13 <cornetj2@gmail.com>
2020-07-22 13:51:22 -0700

    Started new project (Chess Board Game). Defined required classes.

---
+++ ChessBoardConsoleApp/ChessBoardModel/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    class Board 
+    {
+        // Size of the board (usually 8x8).
+        public int Size { get; set; }
+        // 2D array of the grid (type Cell).
+        public Cell[,] theGrid { get; set; }
+
+        // Board constructor.
+        public Board(int size)
+        {
+            // Board size.
+            Size = size;
+
+            // Create a new 2D array (type Cell).
+            theGrid = new Cell[Size, Size];
+
+            // Fill the 2D array with new Cells, each with unique x and y coordinates.
+            for (int i = 0; i < Size; i++) 
+            {
+                for (int j = 0; j < Size; j++) 
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+    }
+}
