2d8336fb44d07b65139e11af4b9ed71b63996d22
cornetj13 <cornetj2@gmail.com>
2020-07-22 19:35:25 -0700

    Finished challenge set for Chess app. Added following features: 1. out of bounds check. 2. Various piece movements. 3. Input for piece type. 4. Input error checks. 5. Better looking Chess board.

--- ChessBoardConsoleApp/ChessBoardConsoleApp/Program.cs
+++ ChessBoardConsoleApp/ChessBoardConsoleApp/Program.cs
@@ -21,8 +21,12 @@ namespace ChessBoardConsoleApp
             Cell currentCell = setCurrentCell();
             currentCell.CurrentlyOccupied = true;
 
+            // Ask the user for the appropriate piece name.
+            Console.WriteLine("Which piece would you like to place on the board? (Bishop, King, Knight, Queen, or Rook)");
+            string pieceString = Console.ReadLine();
+
             // Calcualte all legal moves for the piece.
-            myBoard.MarkNextLegalMoves(currentCell, "Knight");
+            myBoard.MarkNextLegalMoves(currentCell, pieceString);
 
             // Print the chess board displaying the piece and it's legal moves.
             // X will represent the piece occupying the cell.
@@ -39,31 +43,61 @@ namespace ChessBoardConsoleApp
         private static Cell setCurrentCell()
         {
             // Default values for current cell.
-            int currentRow = 4;
-            int currentColumn = 4;
+            int currentRow = 3;
+            int currentColumn = 3;
 
-            // Ask user for x and y coordinates. Return a cell location.
-            Console.WriteLine("Enter the row number");
-            string currentRowString = Console.ReadLine();
-            try 
-            {
-                currentRow = int.Parse(currentRowString);
-            } 
-            catch 
-            {
-                Console.WriteLine("Invalid row number. Defaults to 4.");
-            }
+            // Validity of input boolean.
+            bool validRow = false;
+            bool validColumn = false;
 
-            
-            Console.WriteLine("Enter the column number");
-            string currentColumnString = Console.ReadLine();
-            try 
+            // Ask user for x and y coordinates. Check validity of user input. Return a cell location for valid input.
+            while (validRow == false || validColumn == false) 
             {
-                currentColumn = int.Parse(currentColumnString);
-            } 
-            catch 
-            {
-                Console.WriteLine("Invalid Column number. Defaults to 4.");
+                // Default values for current cell.
+                currentRow = 3;
+                currentColumn = 3;
+
+                // Get user inputs.
+                Console.WriteLine("Enter the row number");
+                string currentRowString = Console.ReadLine();
+                try 
+                {
+                    currentRow = int.Parse(currentRowString) - 1;
+                    if (currentRow >= 0 && currentRow <= myBoard.Size - 3) 
+                    { 
+                        validRow = true; 
+                    } 
+                    else 
+                    { 
+                        Console.WriteLine("Value out of range. Please input valid values."); 
+                    }
+                } 
+                catch 
+                {
+                    Console.WriteLine("Invalid input for row (integer only). Default set to 4.");
+                    validRow = true;
+                }
+
+
+                Console.WriteLine("Enter the column number");
+                string currentColumnString = Console.ReadLine();
+                try 
+                {
+                    currentColumn = int.Parse(currentColumnString) - 1;
+                    if (currentColumn >= 0 && currentColumn <= myBoard.Size - 3)
+                    {
+                        validColumn = true;
+                    } 
+                    else 
+                    {
+                        Console.WriteLine("Value out of range. Please input valid values.");
+                    }
+                } 
+                catch 
+                {
+                    Console.WriteLine("Invalid input for column (integer only). Default set to 4.");
+                    validColumn = true;
+                }
             }
 
             myBoard.theGrid[currentRow, currentColumn].CurrentlyOccupied = true;
@@ -73,34 +107,37 @@ namespace ChessBoardConsoleApp
         private static void printBoard(Board myBoard) 
         {
             // Top edge of board.
-            Console.WriteLine("========");
+            Console.WriteLine("=================================");
+            Console.WriteLine("+---+---+---+---+---+---+---+---+");
 
             // Print the chess board to the console (Note: X = piece, + = legal move, . = empty cell).
-            for (int i = 0; i < myBoard.Size; i++) 
+            for (int i = 0; i < myBoard.Size - 2; i++) 
             {
-                for (int j = 0; j < myBoard.Size; j++)
+                for (int j = 0; j < myBoard.Size - 2; j++)
                 {
                     Cell c = myBoard.theGrid[i, j];
 
                     if (c.CurrentlyOccupied == true) 
                     {
-                        Console.Write("X");
+                        Console.Write("| X ");
                     } 
                     else if (c.LegalNextMove == true) 
                     {
-                        Console.Write("+");
+                        Console.Write("| + ");
                     } 
                     else
                     {
-                        Console.Write(".");
+                        Console.Write("|   ");
                     }
                 }
 
+                Console.Write("|");
                 Console.WriteLine();
+                Console.WriteLine("+---+---+---+---+---+---+---+---+");
             }
 
             // Bottom edge of board.
-            Console.WriteLine("========");
+            Console.WriteLine("=================================");
         }
     }
 }
--- ChessBoardConsoleApp/ChessBoardModel/Board.cs
+++ ChessBoardConsoleApp/ChessBoardModel/Board.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ChessBoardModel
-{
-    public class Board 
-    {
+namespace ChessBoardModel {
+    public class Board {
         // Size of the board (usually 8x8).
         public int Size { get; set; }
         // 2D array of the grid (type Cell).
         public Cell[,] theGrid { get; set; }
 
         // Board constructor.
-        public Board(int size)
+        public Board(int size) 
         {
             // Board size.
-            Size = size;
+            Size = size + 2;
 
             // Create a new 2D array (type Cell).
             theGrid = new Cell[Size, Size];
@@ -33,9 +31,9 @@ namespace ChessBoardModel
         public void MarkNextLegalMoves(Cell currentCell, string chessPiece) 
         {
             // Step 1: Clear all previous legal moves.
-            for (int i = 0; i < Size; i++)
+            for (int i = 0; i < Size; i++) 
             {
-                for (int j = 0; j < Size; j++)
+                for (int j = 0; j < Size; j++) 
                 {
                     theGrid[i, j].LegalNextMove = false;
                     theGrid[i, j].CurrentlyOccupied = false;
@@ -43,29 +41,134 @@ namespace ChessBoardModel
             }
 
             // Step 2: Find all legals moves.
-            switch (chessPiece) 
-            {
+            switch (chessPiece) {
                 case "Knight":
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    // Describes the L movement of the Knight, and checks if it's move is inbound.
+                    if (Inbounds(currentCell.RowNumber + 2, currentCell.ColumnNumber + 1)) 
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 2, currentCell.ColumnNumber - 1)) 
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 2, currentCell.ColumnNumber + 1)) 
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 2, currentCell.ColumnNumber - 1)) 
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 1, currentCell.ColumnNumber + 2)) 
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 1, currentCell.ColumnNumber - 2)) 
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 1, currentCell.ColumnNumber + 2))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 1, currentCell.ColumnNumber - 2)) 
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
                     break;
 
                 case "King":
+                    // Describes the single step movement of the King, and checks if it's move is inbound.
+                    if (Inbounds(currentCell.RowNumber - 1, currentCell.ColumnNumber - 1))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 1, currentCell.ColumnNumber)) 
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber - 1, currentCell.ColumnNumber + 1))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber, currentCell.ColumnNumber - 1)) 
+                    {
+                        theGrid[currentCell.RowNumber, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber, currentCell.ColumnNumber + 1)) 
+                    {
+                        theGrid[currentCell.RowNumber, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 1, currentCell.ColumnNumber - 1)) 
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 1, currentCell.ColumnNumber)) 
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    if (Inbounds(currentCell.RowNumber + 1, currentCell.ColumnNumber + 1)) 
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
                     break;
 
                 case "Rook":
+                    // Describes the horizontal/vertical movement of the Rook, and checks if it's move is inbound.
+                    for (int i = -Size; i < Size; i++) 
+                    {
+                        if (Inbounds(currentCell.RowNumber + i, currentCell.ColumnNumber)) 
+                        {
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        if (Inbounds(currentCell.RowNumber, currentCell.ColumnNumber + i)) 
+                        {
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                    }
                     break;
 
                 case "Bishop":
+                    // Describes the diagonal movement of the Bishop, and checks if it's move is inbound.
+                    for (int i = -Size; i < Size; i++) 
+                    {
+                        if (Inbounds(currentCell.RowNumber + i, currentCell.ColumnNumber + i)) 
+                        {
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                        if (Inbounds(currentCell.RowNumber - i, currentCell.ColumnNumber + i))
+                        {
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                    }
                     break;
 
                 case "Queen":
+                    // Describes the complex movement of the Queen, and checks if it's move is inbound.
+                    for (int i = -Size; i < Size; i++) 
+                    {
+                        if (Inbounds(currentCell.RowNumber + i, currentCell.ColumnNumber)) 
+                        {
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        if (Inbounds(currentCell.RowNumber, currentCell.ColumnNumber + i)) 
+                        {
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                        if (Inbounds(currentCell.RowNumber + i, currentCell.ColumnNumber + i)) 
+                        {
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                        if (Inbounds(currentCell.RowNumber - i, currentCell.ColumnNumber + i)) 
+                        {
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                        }
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid piece");
                     break;
             }
 
@@ -73,6 +176,18 @@ namespace ChessBoardModel
             theGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
 
             // Step 3: Mark the appropriate cells as "Legal".
+        }
+
+        private bool Inbounds(int row, int col) 
+        {
+            if ((row < 0 || row > 8) || (col < 0 || col > 8))
+            {
+                return false;
+            } 
+            else 
+            {
+                return true;
+            }
         }
     }
 }
