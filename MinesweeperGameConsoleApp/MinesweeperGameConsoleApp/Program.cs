aae911c64301a4993030ecf49ab960c17bd92ba9
cornetj13 <cornetj2@gmail.com>
2020-07-24 17:06:52 -0700

    Implemented the initialization of board with bombs and counting live neightbors.

--- MinesweeperGameConsoleApp/MinesweeperGameConsoleApp/Program.cs
+++ MinesweeperGameConsoleApp/MinesweeperGameConsoleApp/Program.cs
@@ -15,6 +15,7 @@ namespace MinesweeperGameConsoleApp
             // Initialize validity check.
             bool validDifficulty = false;
             int boardSize = 0;
+            int percentageBombs = 0;
 
             while (validDifficulty == false) 
             {
@@ -28,18 +29,21 @@ namespace MinesweeperGameConsoleApp
                     Console.WriteLine("You chose easy!");
                     validDifficulty = true;
                     boardSize = 10;
+                    percentageBombs = 8;
                 } 
                 else if (difficultyString == "Medium" || difficultyString == "medium") 
                 {
                     Console.WriteLine("You chose medium!");
                     validDifficulty = true;
                     boardSize = 16;
+                    percentageBombs = 12;
                 } 
                 else if (difficultyString == "Hard" || difficultyString == "hard") 
                 {
                     Console.WriteLine("You chose hard!");
                     validDifficulty = true;
                     boardSize = 20;
+                    percentageBombs = 20;
                 } 
                 else 
                 {
@@ -50,7 +54,13 @@ namespace MinesweeperGameConsoleApp
             // Initialize the minesweeper board.
             Board myBoard = new Board(boardSize);
 
-            // Show the empty Minesweeper board.
+            // Set difficulty
+            myBoard.Difficulty = percentageBombs;
+
+            // Populate the game with bombs.
+            myBoard.PlantBombs();
+
+            // Show the planted Minesweeper board.
             printBoard(myBoard);
 
             // Ends program after key press.
@@ -62,6 +72,8 @@ namespace MinesweeperGameConsoleApp
             // Print the minesweeper board. * are bombs.
             for (int i = 0; i < (myBoard.Size * 2) + 3; i++) 
             {
+                int topEdge = i - 3;
+
                 if (i == 0) 
                 {
                     // Top edge of board.
@@ -94,7 +106,27 @@ namespace MinesweeperGameConsoleApp
                     {
                         for (int j = 0; j < myBoard.Size; j++) 
                         {
-                            Console.Write("|   ");
+                            int k = ((topEdge + 2) / 2);
+
+                            Cell c = myBoard.theGrid[k, j];
+
+                            if (c.ContainsBomb == true) 
+                            {
+                                Console.Write("| * ");
+                            } 
+                            else 
+                            {
+                                c.LiveNeighbors = myBoard.CalculateLiveNeighbors(k, j);
+
+                                if (c.LiveNeighbors == 0) 
+                                {
+                                    Console.Write("|   ");
+                                } 
+                                else 
+                                {
+                                    Console.Write("| " + c.LiveNeighbors + " ");
+                                }
+                            }
                         }
                         Console.Write("|");
                     }
--- MinesweeperGameConsoleApp/MinesweeperModel/Board.cs
+++ MinesweeperGameConsoleApp/MinesweeperModel/Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,21 @@ namespace MinesweeperModel
 {
     public class Board 
     {
+        // Needed for random numbers.
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
         // Size of the board.
         public int Size { get; set; }
 
         // 2D array of the grid (type Cell).
         public Cell[,] theGrid { get; set; }
 
+        // Difficulty of game (decides percentage of bombs in game).
+        public int Difficulty { get; set; }
+
         // Board constructor.
-        public Board(int size) {
+        public Board(int size) 
+        {
             // Board size.
             Size = size;
 
@@ -28,6 +36,118 @@ namespace MinesweeperModel
                     theGrid[i, j] = new Cell(i, j);
                 }
             }
+        }
+
+        public void PlantBombs() 
+        {
+            // Plants bombs randomly on cells.
+            for (int i = 0; i < Size; i++) 
+            {
+                int randomNumber = Between(0, 100);
+
+                for (int j = 0; j < Size; j++) 
+                {
+                    randomNumber = Between(0, 100);
+
+                    if (randomNumber > Difficulty) 
+                    {
+                        theGrid[i, j].ContainsBomb = false;
+                    }
+                    else if (randomNumber <= Difficulty)
+                    {
+                        theGrid[i, j].ContainsBomb = true;
+                    }  
+                }
+            }
+        }
+
+        public int CalculateLiveNeighbors(int row, int col)
+        {
+            int liveCount = 0;
+
+            if (Inbounds(row - 1, col - 1)) {
+                liveCount += CheckIfLive(row - 1, col - 1);
+            }
+
+            if (Inbounds(row - 1, col)) {
+                liveCount += CheckIfLive(row - 1, col);
+            }
+
+            if (Inbounds(row - 1, col + 1)) {
+                liveCount += CheckIfLive(row - 1, col + 1);
+            }
+
+            if (Inbounds(row, col - 1)) {
+                liveCount += CheckIfLive(row, col - 1);
+            }
+
+            if (Inbounds(row, col + 1)) {
+                liveCount += CheckIfLive(row, col + 1);
+            }
+
+            if (Inbounds(row + 1, col - 1)) {
+                liveCount += CheckIfLive(row + 1, col - 1);
+            }
+
+            if (Inbounds(row + 1, col)) {
+                liveCount += CheckIfLive(row + 1, col);
+            }
+
+            if (Inbounds(row + 1, col + 1)) {
+                liveCount += CheckIfLive(row + 1, col + 1);
+            }
+
+            return liveCount;
+
+        }
+
+        private int CheckIfLive(int row, int col)
+        {
+            if (theGrid[row, col].ContainsBomb == true) 
+            {
+                return 1;
+            } 
+            else
+            {
+                return 0;
+            }
+        }
+
+        private bool Inbounds(int row, int col) 
+        {
+            if (row < 0 || row > Size - 1) 
+            {
+                return false;
+            } 
+            else if (col < 0 || col > Size - 1) 
+            {
+                return false;
+            } 
+            else 
+            {
+                return true;
+            }
+        }
+
+        // Random number generator taken from https://scottlilly.com/create-better-random-numbers-in-c/#:~:text=Random%20class%2C%20and%20create%20new,and%20then%20stop%20the%20program.
+        public static int Between(int minimumValue, int maximumValue) {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Math.Max, and substracting 0.00000000001, 
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
         }
     }
 }
